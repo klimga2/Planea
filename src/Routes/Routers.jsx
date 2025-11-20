e3e473341786ef896d1d63bfb5bfad520b6aba93
duque.murillas <duque.murillas@gmail.com>
2025-11-20 18:17:27 +0000

    Fix merge conflicts in Routers.jsx

--- src/Routes/Routers.jsx
+++ src/Routes/Routers.jsx
@@ -3,18 +3,13 @@ import Dashboard from '../Pages/Dashboard/Dashboard - Inicio';
 import Presupuesto from '../Pages/Dashboard/Presupuesto';
 import GastosMes from '../Pages/Dashboard/Distribución gastos x mes';
 import GastosSemana from '../Pages/Dashboard/Distribución gastos x semana';
-<<<<<<< HEAD
+import Simuladores from '../Pages/Dashboard/Simuladores';
 import MigestiongestionDiaria from '../Pages/Migestion/Migestion-gestionDiaria';
 import MigestionMovimientos from '../Pages/Migestion/Migestion-gestionDiariaMovimient';
 import MigestionPresupuesto from '../Pages/Migestion/Migestion-presupuesto';
 import MigestionGastosFijos from '../Pages/Migestion/Migestion-Gastosfijos';
 import MigestionGastosFijosAdmin from '../Pages/Migestion/Migestion-Gastosfijos-admin';
-=======
-import Simuladores from '../Pages/Dashboard/Simuladores';
-import MigestiongestionDiaria from '../Pages/Migestion/Migestion-gestionDiaria';
-import MigestionMovimientos from '../Pages/Migestion/Migestion-gestionDiariaMovimient';
-import MigestionPresupuesto from '../Pages/Migestion/Migestion-presupuesto';
->>>>>>> main
+
 const AppRouter = () => {
 	return (
 		<BrowserRouter>
@@ -23,18 +18,12 @@ const AppRouter = () => {
 				<Route path='/GastosMes' element={<GastosMes />} />
 				<Route path='/GastosSemana' element={<GastosSemana />} />
 				<Route path='/Presupuesto' element={<Presupuesto />} />
-<<<<<<< HEAD
+				<Route path='/Simuladores' element={<Simuladores />} />
 				<Route path='/Migestion-gestionDiaria' element={<MigestiongestionDiaria />} />
 				<Route path='/Migestion-gestionDiariaMovimientos' element={<MigestionMovimientos />} />
 				<Route path='/Migestion-presupuesto' element={<MigestionPresupuesto />} />
 				<Route path='/Migestion-gastos-fijos' element={<MigestionGastosFijos />} />
 				<Route path='/Migestion-Gastosfijos-admin' element={<MigestionGastosFijosAdmin />} />
-=======
-				<Route path='/Simuladores' element={<Simuladores />} />
-				<Route path='/Migestion-gestionDiaria' element={<MigestiongestionDiaria />} />
-				<Route path='/Migestion-gestionDiariaMovimientos' element={<MigestionMovimientos />} />
-				<Route path='/Migestion-presupuesto' element={<MigestionPresupuesto />} />
->>>>>>> main
 			</Routes>
 		</BrowserRouter>
 	);
